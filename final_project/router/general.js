1f3e346b19af07cfc00877b02d9b983e0b9d2a91
Deepa <deepaalingal922@gmail.com>
2024-01-19 09:50:11 -0500

    1.Getting all list of books.2)Getting books by ISBN number. 3) Getting book by author. 4)Getting book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,24 +13,59 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ // return res.status(300).json({message: "Yet to be implemented"});
+ res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  const isbn = req.params.isbn;
+  res.send(JSON.stringify(books[isbn],null,4));
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+
+
+// Filter books by author
+console.log("Output: ", author);
+//const bookValues = allBooks.filter(book => book.author.toLowerCase() === author);
+
+// Convert object values to an array
+const allBooks = Object.entries(books);
+const bookKeys = Object.keys(books);
+
+   // Iterate through the 'books' object & check the author matches the one provided
+   const booksByAuthor = bookKeys
+      .filter(key => books[key].author.toLowerCase() === author.toLowerCase())
+      .map(key => books[key]);
+
+   console.log(`Books by ${author}:`, booksByAuthor);
+
+res.send(booksByAuthor);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
+  const title = req.params.title;
+
+  // Convert object values to an array
+const allBooks = Object.entries(books);
+const bookKeys = Object.keys(books);
+
+   // Iterate through the 'books' object & check the author matches the one provided
+   const booksByTitle = bookKeys
+      .filter(key => books[key].title.toLowerCase() === title.toLowerCase())
+      .map(key => books[key]);
+
+   console.log(`Books by ${title}:`, booksByTitle);
+
+res.send(booksByTitle);
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
